f126dfa385f7a960c5e4fc2224f8a34ad0d80082
Aida <69685654+kvrgic@users.noreply.github.com>
2023-07-07 11:19:29 +0200

    final project HTML and CSS

---
+++ FinalProjectHTML&CSS/script.js
@@ -0,0 +1,15 @@
+var dayOption = document.getElementById('birthday-select-day');
+
+for(var i = 1; i <= 31; i++){
+    var optionD = document.createElement('option');
+    optionD.text = i;
+    dayOption.add(optionD);
+}
+
+var yearOption = document.getElementById('birthday-select-year');
+
+for(var i = 1960; i <= 2010; i++){
+    var optionY = document.createElement('option');
+    optionY.text = i;
+    yearOption.add(optionY);
+}
