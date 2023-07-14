dd3742c0ca6f602ad452bb1c2622ea6cfa9bfb87
Aida <69685654+kvrgic@users.noreply.github.com>
2023-07-14 10:01:23 +0200

    modified project

--- SIGN IN/script.js
+++ SIGN IN/script.js
@@ -1,4 +1,4 @@
-/* array of users*/
+/* Array of users*/
 var users = [
     {
         name: 'Aida Kvrgic',
@@ -9,32 +9,55 @@ var users = [
     },
 ]
 
+/* Array - all posted blogs*/
 var allBlogs = [];
 
+/* Logged user */
 var loggedUser = {};
 
+/* Function for get value and clear value of element */
 function getValue(id){
     return document.getElementById(id).value;
 }
+function clearValue(id){
+    document.getElementById(id).value = '';
+}
 
-var signinform = document.getElementById('signin-form');
-signinform.style.display = 'block';
-var signupform = document.getElementById('signup-form');
-signupform.style.display = 'none';
-var navigation = document.getElementById('nav');
-var showEmail = document.querySelector('.show-name');
-var incorectMess = document.getElementById('incorect-mess');
+/* If user logged - exists in localStorage, when we refresh the page - user stay logged */
+function isUserLogged(){
+    var userData = localStorage.getItem('loggedUser');
 
-function signIn(){
-    var email = getValue('email-input');
-    var password = getValue('password-input');
+    if(userData){
+        signIn(JSON.parse(userData));
+    }
+}
+isUserLogged();
+
+/* Sign in function */
+function signIn(user){
+    var email = user.email || getValue('email-input');
+    var password = user.password || getValue('password-input');
+
+    var signinform = document.getElementById('signin-form');
+    var navigation = document.getElementById('nav');
+    var showEmail = document.querySelector('.show-name');
+    var incorectMess = document.getElementById('incorect-mess');
+
+    /* Get data from the local storage */
+    var usersData = localStorage.getItem('users');
+    if(usersData){
+        users = JSON.parse(usersData)
+    }
 
     for(var oneUser of users){
         if((email === oneUser.email || email === oneUser.username) && password === oneUser.password){
             signinform.style.display = 'none';
             navigation.style.display = 'block'
-            showEmail.innerHTML = oneUser.name;
+
             loggedUser = oneUser;
+            localStorage.setItem('loggedUser', JSON.stringify(loggedUser))
+            
+            showEmail.innerHTML = loggedUser.name;
             clearValue('email-input');
             clearValue('password-input');
         }else{
@@ -43,24 +66,41 @@ function signIn(){
     } 
 }
 
+/* Sign out function*/
 function signOut(){
+    var navigation = document.getElementById('nav');
+    var signinform = document.getElementById('signin-form');
+    var incorectMess = document.getElementById('incorect-mess');
+
     navigation.style.display = 'none';
     signinform.style.display = 'block';
     incorectMess.style.display = 'none';
+
+    localStorage.removeItem('loggedUser');
 
     clearValue('blog-title');
     clearValue('blog-story');
 }
 
+/* Function on button SIGNUP - go to sign up form*/
 function goToSignUpForm(){
+    var signinform = document.getElementById('signin-form');
+    var signupform = document.getElementById('signup-form');
+
     signinform.style.display = 'none';
     signupform.style.display = 'block';
 }
+
+/* Function on button SIGNIN - go to sign in form*/
 function goToSignInForm(){
+    var signinform = document.getElementById('signin-form');
+    var signupform = document.getElementById('signup-form');
+
     signinform.style.display = 'block';
     signupform.style.display = 'none';
 }
 
+/* User ragistration, get input value, push to users array and local storage, clear input value, and call function goToSignInForm */
 function signUp(){
     var name = getValue('r-name-input');
     var email = getValue('r-email-input');
@@ -79,6 +119,8 @@ function signUp(){
         password
     };
     users.push(user);
+    localStorage.setItem('users', JSON.stringify(users))
+
     clearValue('r-name-input');
     clearValue('r-email-input');
     clearValue('r-address-input');
@@ -86,12 +128,11 @@ function signUp(){
     clearValue('r-password-input');
     goToSignInForm()
 }
-function clearValue(id){
-    document.getElementById(id).value = '';
-}
+
+/* Function on all signIn or signUp input - logged or registration with enter key */
 function enterSignIn(event){
     if(event.keyCode === 13){
-        signIn();
+        signIn({});
     }
 }
 function regBackToSignIn(event){
@@ -100,6 +141,7 @@ function regBackToSignIn(event){
     }
 }
 
+/* function on button POST BLOG, get title and blog story value, push value in blog object, push object in allBlogs array, call displayBlogs function and clear title and blog story input*/
 function postNewBlog(){
     var titleOfBlog = getValue('blog-title')
     var storyOfBlog = getValue('blog-story');
@@ -122,6 +164,7 @@ function postNewBlog(){
     clearValue('blog-story');
 }
 
+/* Get div element from the html and clean div, use one blog of the allBlogs array, create a new div for the post, title, post story, date and name of user posting the blog, appendChild to the html div */
 function displayBlog(){
     var displayBlogs = document.querySelector('.posted-blog');
     displayBlogs.innerHTML = '';
